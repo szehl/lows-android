82731261a94bd1969305a356d7e97972fb7c84e1
Sven Zehl <zehl@tu-berlin.de>
2017-09-22 12:06:38 +0200

    Conference Demo Mobicom

--- LoW-S/app/src/main/java/com/lows/MobicomDemoCodebook.java
+++ LoW-S/app/src/main/java/com/lows/MobicomDemoCodebook.java
@@ -165,13 +165,141 @@ public class MobicomDemoCodebook extends PreinstalledCodebook {
         autoAddEntry("CK1", "Making Roads Safer by Making Drivers Better\n" +
                 "Hari Balakrishnan (M.I.T. and Cambridge Mobile Telematics) ,  09:05 - 09:55");
 
+        autoAddEntry("C[C", "Coffee Break");
+        autoAddEntry("C]C", "Coffee Break");
+        autoAddEntry("C}C", "Coffee Break");
+        autoAddEntry("CC1", "Coffee Break 09:55 - 10:20");
 
+        autoAddEntry("C[S", "Paper session I: Wireless High Jinks  10:20 - 11:40");
+        autoAddEntry("C]S", "Paper session I: Wireless High Jinks  10:20 - 11:40");
+        autoAddEntry("C}S", "Paper session I: Wireless High Jinks  10:20 - 11:40");
+        autoAddEntry("CS1", "Paper session I: Wireless High Jinks  10:20 - 11:40");
 
-        autoAddEntry("CS1", "Paper session I: Wireless High Jinks");
-        autoAddEntry("Ct1", "WEBee: Physical-Layer Cross-Technology Communication via Emulation\n" +
+        autoAddEntry("CT1", "WEBee: Physical-Layer Cross-Technology Communication via Emulation\n" +
                 "Zhijun Li (University of Minnesota & Harbin Institute of Technology)\n" +
                 "Tian He (University of Minnesota)\n");
+        autoAddEntry("CT2", "Stateful Inter-Packet Signal Processing for Wireless Networking\n" +
+                "Shangqing Zhao (University of South Florida)\n" +
+                "Zhengping Luo (University of South Florida)\n" +
+                "Zhuo Lu (University of South Florida)\n" +
+                "Xiang Lu (Chinese Academy of Sciences & University of Chinese Academy of Sciences)\n" +
+                "Yao Liu (University of South Florida)\n");
+        autoAddEntry("CT3", "WWiFi-Assisted 60 GHz Wireless Networks\t\n" +
+                "Sanjib Sur (University of Wisconsin-Madison & Hewlett Packard Labs)\n" +
+                "Ioannis Pefkianakis (Hewlett Packard Labs)\n" +
+                "Xinyu Zhang (University of California, San Diego)\n" +
+                "Kyu-Han Kim (Hewlett Packard Labs)");
+        autoAddEntry("CT4", "Pose Information Assisted 60 GHz Networks: Towards Seamless Coverage and Mobility Support\n" +
+                "Teng Wei (University of Wisconsin - Madison)\n" +
+                "Xinyu Zhang (University of California San Diego) ");
 
+        autoAddEntry("C[L", "Lunch");
+        autoAddEntry("C]L", "Lunch");
+        autoAddEntry("C}L", "Lunch");
+        autoAddEntry("CL1", "Lunch 11:40 - 13:10");
+
+        autoAddEntry("C[N", "N2Women Event 12:00 - 13:00");
+        autoAddEntry("C]N", "N2Women Event 12:00 - 13:00");
+        autoAddEntry("C}N", "N2Women Event 12:00 - 13:00");
+        autoAddEntry("CN1", "N2Women Event 12:00 - 13:00");
+
+        autoAddEntry("C[T", "Paper session II: Can You Hear Me Now? 13:10 - 14:30");
+        autoAddEntry("C]T", "Paper session II: Can You Hear Me Now? 13:10 - 14:30");
+        autoAddEntry("C}T", "Paper session II: Can You Hear Me Now? 13:10 - 14:30");
+        autoAddEntry("CS2", "Paper session II: Can You Hear Me Now? 13:10 - 14:30");
+
+        autoAddEntry("CT5", "A Control-Plane Perspective on Reducing Data Access Latency in LTE Networks\n" +
+                "Yuanjie Li (University of California, Los Angeles)\n" +
+                "Zengwen Yuan (University of California, Los Angeles)\n" +
+                "Chunyi Peng (Purdue University)");
+
+        autoAddEntry("CT6", "Experience: An Open Platform for Experimentation with Commercial Mobile Broadband Networks\n" +
+                "Özgü Alay (Simula Research Laboratory)\n" +
+                "Andra Lutu (Simula Research Laboratory)\n" +
+                "Miguel Peón-Quirós (IMDEA Networks Institute)\n" +
+                "Vincenzo Mancuso (IMDEA Networks Institute)\n" +
+                "Thomas Hirsch (Celerway Communications)\n" +
+                "Kristian Evensen (Celerway Communications)\n" +
+                "Audun Hansen (Celerway Communications)\n" +
+                "Stefan Alfredsson (Karlstad University)\n" +
+                "Jonas Karlsson (Karlstad University)\n" +
+                "Anna Brunstrom (Karlstad University)\n" +
+                "Ali Safari Khatouni (Politecnico di Torino)\n" +
+                "Marco Mellia (Politecnico di Torino)\n" +
+                "Marco Ajmone Marsan (IMDEA Networks Institute & Politecnico di Torino)\n");
+
+        autoAddEntry("CT7", "Experience: Automating Diagnosis of Cellular Radio Access Network Problems\n" +
+                "Anand Padmanabha Iyer (University of California, Berkeley)\n" +
+                "Li Erran Li (Uber Technologies)\n" +
+                "Ion Stoica (University of California, Berkeley)\n");
+
+        autoAddEntry("CT8", "Adding the Next Nine: an Investigation of Mobile Broadband Networks Availability\t\n" +
+                "Ahmed Elmokashfi (Simula Research Laboratory)\n" +
+                "DongZhou (Simula Research Laboratory)\n" +
+                "Džiugas Baltrunas (Simula Research Laboratory)");
+
+        autoAddEntry("CC2", "Coffee Break 14:30 - 14:55");
+
+        autoAddEntry("C[U", "Paper session III: Invisible Cobwebs 14:55 - 16:35");
+        autoAddEntry("C]U", "Paper session III: Invisible Cobwebs 14:55 - 16:35");
+        autoAddEntry("C}U", "Paper session III: Invisible Cobwebs 14:55 - 16:35");
+        autoAddEntry("CS3", "Paper session III: Invisible Cobwebs 14:55 - 16:35");
+
+        autoAddEntry("CT9", "The Tick Programmable Low-Latency SDR System\n" +
+                "Haoyang Wu (Peking University)\n" +
+                "Tao Wang (Peking University)\n" +
+                "Zengwen Yuan (University of California, Los Angeles)\n" +
+                "Chunyi Peng (Purdue University)\n" +
+                "Zhiwei Li (Peking University)\n" +
+                "Zhaowei Tan (University of California, Los Angeles)\n" +
+                "Boyan Ding (Peking University)\n" +
+                "Xiaoguang Li (Peking University)\n" +
+                "Yuanjie Li (University of California, Los Angeles)\n" +
+                "Jun Liu (Peking University)\n" +
+                "Songwu Lu (University of California, Los Angeles)");
+
+        autoAddEntry("CTA", "BiPass: Enabling End-to-End Full Duplex\t\n" +
+                "Lu Chen (Ohio State University)\n" +
+                "Fei Wu (Ohio State University)\n" +
+                "Jiaqi Xu (Ohio State University)\n" +
+                "Kannan Srinivasan (Ohio State University)\n" +
+                "Ness Shroff (Ohio State University) ");
+
+        autoAddEntry("CTB", "Orion: RAN Slicing for a Flexible and Cost-Effective Multi-Service Mobile Network Architecture\t\n" +
+                "Xenofon Foukas (University of Edinburgh)\n" +
+                "Mahesh K. Marina (University of Edinburgh)\n" +
+                "Kimon Kontovasilis (NCSR Demokritos)");
+
+        autoAddEntry("CTC", "Accelerating Multipath Transport Through Balanced Subflow Completion\n" +
+                "Yihua Ethan Guo (University of Michigan)\n" +
+                "Ashkan Nikravesh (University of Michigan)\n" +
+                "Z. Morley Mao (University of Michigan)\n" +
+                "Feng Qian (Indiana University)\n" +
+                "Subhabrata Sen (AT&T Labs – Research)");
+
+        autoAddEntry("CTD", "FSONet: A Wireless Backhaul for Multi-Gigabit Picocells Using Steerable Free Space Optics\n" +
+                "Max Curran (Stony Brook University)\n" +
+                "Md. Shaifur Rahman (Stony Brook University)\n" +
+                "Himanshu Gupta (Stony Brook University)\n" +
+                "Kai Zheng (Stony Brook University)\n" +
+                "Jon Longtin (Stony Brook University)\n" +
+                "Samir R Das (Stony Brook University)\n" +
+                "Thanvir Mohamed (Stony Brook University)");
+
+        autoAddEntry("C[A", "App Contest 16:35-17:35");
+        autoAddEntry("C]A", "App Contest 16:35-17:35");
+        autoAddEntry("C}A", "App Contest 16:35-17:35");
+        autoAddEntry("CX2", "App Contest 16:35-17:35");
+
+        autoAddEntry("C[D", "Demo/poster session and Student Research Competition (SRC) with reception 17:35 - 19:15");
+        autoAddEntry("C]D", "Demo/poster session and Student Research Competition (SRC) with reception 17:35 - 19:15");
+        autoAddEntry("C}D", "Demo/poster session and Student Research Competition (SRC) with reception 17:35 - 19:15");
+        autoAddEntry("CD1", "Demo/poster session and Student Research Competition (SRC) with reception 17:35 - 19:15");
+
+        autoAddEntry("C[J", "MobiJob dinner 19:20 - 21:20");
+        autoAddEntry("C]J", "MobiJob dinner 19:20 - 21:20");
+        autoAddEntry("C}J", "MobiJob dinner 19:20 - 21:20");
+        autoAddEntry("CX3", "MobiJob dinner 19:20 - 21:20");
 
     }
 }
--- demo/mobicom.sh
+++ demo/mobicom.sh
@@ -61,5 +61,182 @@ execute_on_date "2017-10-17 09:04" "^CXW^^C}K^"
 #Keynote1 09:05 - 09:55
 execute_on_date "2017-10-17 09:05" "^CK1^"
 
+#Break in 5min
+execute_on_date "2017-10-17 09:50" "^CK1^^C[C^"
+
+#Break in 3min
+execute_on_date "2017-10-17 09:52" "^CK1^^C]C^"
+
+#Break in 1min
+execute_on_date "2017-10-17 09:54" "^CK1^^C}C^"
+
+#Break 09:05 - 09:55
+execute_on_date "2017-10-17 09:55" "^CC1^"
+
+#1st Session in 5min
+execute_on_date "2017-10-17 10:15" "^CC1^^C[S^"
+
+#1st Session in 3min
+execute_on_date "2017-10-17 10:17" "^CC1^^C]S^"
+
+#1st Session in 1min
+execute_on_date "2017-10-17 10:19" "^CC1^^C}S^"
+
+#Paper session I: Wireless High Jinks  10:20 - 11:40
+execute_on_date "2017-10-17 10:20" "^CS1^"
+
+#Talk 1
+execute_on_date "2017-10-17 10:20" "^CS1^^CT1^"
+
+#Talk 2
+execute_on_date "2017-10-17 10:40" "^CS1^^CT2^"
+
+#Talk 3
+execute_on_date "2017-10-17 11:00" "^CS1^^CT3^"
+
+#Talk 4
+execute_on_date "2017-10-17 11:20" "^CS1^^CT4^"
+
+#Lunch in 5min
+execute_on_date "2017-10-17 11:35" "^CS1^^CT4^^C[L^"
+
+#Lunch in 3min
+execute_on_date "2017-10-17 11:37" "^CS1^^CT4^^C]L^"
+
+#Lunch in 1min
+execute_on_date "2017-10-17 11:39" "^CS1^^CT4^^C}L^"
+
+#Lunch 11:40 - 13:10
+execute_on_date "2017-10-17 11:40" "^CL1^"
+
+#N2Women in 5min
+execute_on_date "2017-10-17 11:55" "^CL1^^C[N^"
+
+#N2Women in 3min
+execute_on_date "2017-10-17 11:57" "^CL1^^C]N^"
+
+#N2Women in 1min
+execute_on_date "2017-10-17 11:59" "^CL1^^C}N^"
+
+#N2Women 12:00 - 13:00 + Lunch
+execute_on_date "2017-10-17 12:00" "^CN1^^CL1^"
+
+#Only Lunch till 13:10
+execute_on_date "2017-10-17 13:00" "^CL1^"
+
+#Session 2 in 5min
+execute_on_date "2017-10-17 13:05" "^CL1^^C[T^"
+
+#Session 2 in 3min
+execute_on_date "2017-10-17 13:07" "^CL1^^C]T^"
+
+#Session 2 in 1min
+execute_on_date "2017-10-17 13:09" "^CL1^^C}T^"
+
+#Paper session II: Can You Hear Me Now? 13:10 - 14:30
+#+talk 5
+execute_on_date "2017-10-17 13:10" "^CS2^^CT5^"
+
+#Paper session II: Can You Hear Me Now? 13:10 - 14:30
+#+talk 6
+execute_on_date "2017-10-17 13:30" "^CS2^^CT6^"
+
+#Paper session II: Can You Hear Me Now? 13:10 - 14:30
+#+talk 7
+execute_on_date "2017-10-17 13:50" "^CS2^^CT7^"
+
+#Paper session II: Can You Hear Me Now? 13:10 - 14:30
+#+talk 8
+execute_on_date "2017-10-17 14:10" "^CS2^^CT8^"
+
+#Break in 5min
+execute_on_date "2017-10-17 14:25" "^CS2^^CT8^^C[C^"
+
+#Break in 3min
+execute_on_date "2017-10-17 14:27" "^CS2^^CT8^^C]C^"
+
+#Break in 1min
+execute_on_date "2017-10-17 14:29" "^CS2^^CT8^^C}C^"
+
+#Break2 14:30 - 14:55
+execute_on_date "2017-10-17 14:30" "^CC2^"
+
+#Session 3 in 5min
+execute_on_date "2017-10-17 14:50" "^CC2^^C[U^"
+
+#Session 3 in 3min
+execute_on_date "2017-10-17 14:52" "^CC2^^C]U^"
+
+#Session 2 in 1min
+execute_on_date "2017-10-17 14:54" "^CC2^^C}U^"
+
+#Paper session II: Paper session III: Invisible Cobwebs 14:55 - 16:35
+#+talk 9 
+execute_on_date "2017-10-17 14:55" "^CS3^^CT9^"
+
+#Paper session II: Paper session III: Invisible Cobwebs 14:55 - 16:35
+#+talk 10 
+execute_on_date "2017-10-17 15:15" "^CS3^^CTA^"
+
+#Paper session II: Paper session III: Invisible Cobwebs 14:55 - 16:35
+#+talk 11
+execute_on_date "2017-10-17 15:35" "^CS3^^CTB^"
+
+#Paper session II: Paper session III: Invisible Cobwebs 14:55 - 16:35
+#+talk 12
+execute_on_date "2017-10-17 15:55" "^CS3^^CTC^"
+
+#Paper session II: Paper session III: Invisible Cobwebs 14:55 - 16:35
+#+talk 13
+execute_on_date "2017-10-17 16:15" "^CS3^^CTD^"
+
+
+#AppContest in 5min
+execute_on_date "2017-10-17 16:30" "^CS3^^CTD^^C[A^"
+
+#App Contest in 3min
+execute_on_date "2017-10-17 16:32" "^CS3^^CTD^^C]A^"
+
+#App Contest in 1min
+execute_on_date "2017-10-17 16:34" "^CS3^^CTD^^C}A^"
+
+#App Contest 16:35-17:35
+execute_on_date "2017-10-17 16:35" "^CX2^"
+
+#Demo Session in 5min
+execute_on_date "2017-10-17 17:30" "^CX2^^C[D^"
+
+#Demo Session in 3min
+execute_on_date "2017-10-17 17:32" "^CX2^^C]D^"
+
+#Demo Session in 1min
+execute_on_date "2017-10-17 17:34" "^CX2^^C}D^"
+
+#Demo Session 17:35 - 19:15
+execute_on_date "2017-10-17 17:35" "^CD1^"
+
+
+#MobiJob Starting in 5min
+execute_on_date "2017-10-17 19:15" "^C[J^"
+
+#MobiJob Starting in 3min
+execute_on_date "2017-10-17 19:17" "^C]J^"
+
+#Mobi Job Starting in 1min
+execute_on_date "2017-10-17 19:19" "^C}J^"
+
+#MobiJob dinner 19:20-21:20
+execute_on_date "2017-10-17 19:20" "^CX3^"
+
+#EOB
+execute_on_date "2017-10-17 21:20" "LoWS"
+
+
+
+
+
+
+
  
+
 
